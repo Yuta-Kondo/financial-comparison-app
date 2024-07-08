88fb6f3414e6b736a83d93bae37a87559ac3dcae
Yuta-Kondo <kondoyutah15@gmail.com>
2024-07-07 23:32:19 -0400

    environemntal variable back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,10 +27,7 @@ function App() {
     try {
       console.log("fetching comparison data");
       setError(null);
-      // const response = await axios.get(`${REACT_APP_API_URL}/compare`, {
-      //   params: { symbols: symbolsToFetch },
-      // });
-      const response = await axios.get(`https://financial-comparison-backend.onrender.com/compare`, {
+      const response = await axios.get(`${REACT_APP_API_URL}/compare`, {
         params: { symbols: symbolsToFetch },
       });
       console.log("response: ", response);
